b27584ebc6e1737a4eca308768078ce7dd4f5f25
Etini Alphonsus Alfred <etinialfred@gmail.com>
2023-09-02 12:34:47 +0100

    Creat variables

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
+//Create variable to accept birth year and current year
 let a = parseInt(prompt("birth year"));
 let b = parseInt(prompt("current year"));
-
-
-function currentage() {
-return a - b;
-
-}
-console.log(currentage());
+//Create a subtraction function
+//The subtraction function to return the a number fo
