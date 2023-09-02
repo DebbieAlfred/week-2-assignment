1fb704655e746261d3060b5377ad251ac29d2945
Etini Alphonsus Alfred <etinialfred@gmail.com>
2023-09-02 22:21:08 +0100

    creat a psuedo code for my calculator

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //Create variable to accept birth year and current year
-let a = parseInt(prompt("birth year"));
-let b = parseInt(prompt("current year"));
 //Create a subtraction function
-//The subtraction function to return the a number fo
+//The subtraction function to return the a number for birth year and current year
+//invoke subtraction function to run the calculator
